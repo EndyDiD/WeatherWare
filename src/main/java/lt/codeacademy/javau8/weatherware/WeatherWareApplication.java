b938aeaecc88f6bc75babeb213ed06ad42394420
endydid <Enderguy.EE@gmail.com>
2024-02-13 13:44:16 +0200

    Initial commit: Project Start

---
+++ src/main/java/lt/codeacademy/javau8/weatherware/WeatherWareApplication.java
@@ -0,0 +1,13 @@
+package lt.codeacademy.javau8.weatherware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherWareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherWareApplication.class, args);
+	}
+
+}
