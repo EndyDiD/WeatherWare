ece09afb78e969a92d2eea16d1354bf9fc13fc76
endydid <Enderguy.EE@gmail.com>
2024-02-19 04:25:18 +0200

    Small: Made the page prettier

--- src/main/resources/templates/ThermometerUnitConversion/UnitConverter.js
+++ src/main/resources/templates/ThermometerUnitConversion/UnitConverter.js
@@ -11,7 +11,7 @@ function convertTemperature() {
 
     if (document.getElementById("convert").checked) {
       // Convert from Celsius to Fahrenheit
-      temperatureElements[i].textContent = parseInt((celsiusTemperatures[i] * 9 / 5) + 32).toFixed(0);
+      temperatureElements[i].textContent = Math.floor((celsiusTemperatures[i] * 9 / 5) + 32);
 
       unitElements[i].textContent = "℉";
     } else {
